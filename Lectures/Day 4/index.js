eacfda93fb05e1a71e9da8dd84807ecee2148639
Ketan Shetge <ketanshetgedev@gmail.com>
2026-01-10 10:09:27 +0530

    add Day 4 Javascript basics practrice

---
+++ Lectures/Day 4/index.js
@@ -0,0 +1,152 @@
+//  operator
+
+// Arithmetic 
+
+// console.log(2 + 5)
+// console.log(2 - 5)
+// console.log(2 * 5)
+// console.log(6 / 2)
+// console.log(5 % 2) 
+
+// console.log(5 ** 3)
+
+/****************************************************************************** */
+
+// Assignment Operator 
+
+// let x = 20;
+// let y = 10;
+
+// // x = x + y;
+// // x = x - y
+// // x = x * y
+
+// // x += y
+// // x -= y
+// // x *= y
+// console.log(x)
+
+/****************************************************************************** */
+
+// Comparison Operator 
+
+// let x = 20;
+// let y = 10;
+
+// console.log(x > y)
+// console.log(x < y)
+
+// console.log(x >= y)
+// console.log(x <= y)
+
+// console.log(x == y)
+// console.log(x === y)
+
+/****************************************************************************** */
+
+// let z = "10"
+// let a = Number(z)
+
+// console.log(typeof a, a)
+
+//  string ---> Number
+// let a = "121ac";
+// let b = Number(a);
+
+// console.log(typeof b)
+// console.log(b)
+// console.log(0/0)
+
+//  NaN : Not a number (Type of NaN is number)
+
+/****************************************************************************** */
+
+// Number ---> String 
+// let a = 20;
+// let b = String(a)
+
+// console.log(typeof b , b)
+
+/****************************************************************************** */
+
+// Boolean ---> Number 
+// console.log(Number(true))
+// console.log(Number(false))
+
+/****************************************************************************** */
+
+// // Null ---> Number 
+// console.log(Number(null)) /** 0 */
+
+// // undefined ---> number 
+// console.log(Number(undefined)) /** NaN **/
+
+/****************************************************************************** */
+
+// Boolean --> String 
+// console.log(typeof String(true), String(true))
+// console.log(typeof String(false), String(false))
+
+// console.log(typeof String(null), String(null))
+// console.log(typeof String(undefined), String(undefined))
+
+/****************************************************************************** */
+
+// Number, string, null, undefined ---> Boolean 
+
+// console.log(Boolean(10))
+// console.log(Boolean(0))
+// console.log(Boolean(-30))
+
+// console.log(Boolean("yes"))
+// console.log(Boolean(""))
+// console.log(Boolean(""))
+
+// console.log(Boolean(undefined))
+// console.log(Boolean(null))
+
+/****************************************************************************** */
+
+// computer science challenge
+
+// let a = 0.1;
+// let b = 0.2;
+
+// let c = a + b;
+
+// console.log(c, c == 0.3)
+
+
+/****************************************************************************** */
+
+
+// 1. null is loosely equal to undefined 
+
+// console.log(null == undefined);
+// console.log(null === undefined);
+
+// console.log(null == 0);
+// console.log(null == "");
+// console.log(null == false);
+
+// >, <, >=, <= (null ---> number, undefined ---> NaN)
+
+// console.log(null >= 0)
+// console.log(null <= 0)
+// console.log(null > 0)
+// console.log(null < 0)
+
+// console.log(null >= undefined)
+// console.log(undefined >= 0)
+
+
+/****************************************************************************** */
+
+// console.log("Ketan" > "Chetan")
+
+// console.log(10 > "10")
+// console.log(10 >= "10")
+
+
+// console.log(NaN == "NaN")
+// console.log(NaN === "NaN")
