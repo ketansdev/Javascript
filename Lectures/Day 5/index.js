915460811edfe13db207f460695fa97a7736258d
Ketan Shetge <ketanshetgedev@gmail.com>
2026-01-11 18:44:05 +0530

    add Day 5 basics of Numbers and Math object

---
+++ Lectures/Day 5/index.js
@@ -0,0 +1,61 @@
+// Number 
+
+// let a = 10
+// console.log(a)
+
+// let b = 44.67854
+// let c = b.toFixed(1)
+// console.log(c, typeof c)
+
+// console.log(b.toPrecision(2))
+// console.log(b.toString())
+
+
+/****************************************************** */
+
+
+// let a = new Number(20);  /* not recommended */
+// console.log(a, typeof a) /* object */
+
+// let b = a;
+// console.log(a == b)
+
+
+/****************************************************** */
+
+// console.log( Boolean(0))
+// console.log( Boolean(10))
+// console.log( Boolean(new Number(20)))
+// console.log( Boolean({}))
+// console.log(Boolean(null))
+
+
+/****************************************************** */
+
+// Non Primitive : compared based on References
+// Primitive : Copy by value
+//  eg : 
+
+// let a = 10;
+// b = a
+// console.log(a == b)
+
+
+/****************************************************** */
+
+
+// console.log(Math.abs(-4));
+// console.log(Math.PI);
+// console.log(Math.LN10);
+// console.log(Math.ceil(2.3));
+// console.log(Math.floor(2.3));
+
+// console.log(Math.floor(-3.4))
+// console.log(Math.ceil(-3.4))
+
+// console.log(Math.max(20,400, 201, 45))
+
+// console.log(Math.random()) /* Generates random value between 0 - 1 , where 0 is included but not 1 */
+
+// console.log(Math.floor(Math.random()*10))
+// console.log((Math.random()*10).toPrecision(1))
