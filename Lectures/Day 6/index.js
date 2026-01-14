371a249d6d4a19175bd0b43e398719634a94e399
Ketan Shetge <ketanshetgedev@gmail.com>
2026-01-14 06:48:19 +0530

    add Day 6 - basic strings practice

---
+++ Lectures/Day 6/index.js
@@ -0,0 +1,63 @@
+// const str1 = "Ketan";
+// const str2 = 'Ketan Shetge'
+// const day = 14
+
+// const str3 = `I am learning Javascript,
+// i am learning strings in JS on ${day}`
+
+// console.log(str3)
+
+// const str = `Hello Javascript`
+
+// console.log(str.length)
+// console.log(str[4])
+
+// uppercase
+// const a = str.toUpperCase()
+// const b = str.toLowerCase()
+// console.log(a)
+// console.log(b)
+
+// const str = `Hello Javascript, I am learning Javascript strings on 14`
+
+// console.log(str.indexOf("Java"))
+// console.log(str.indexOf("java"))
+
+// console.log(str.lastIndexOf("Javas"))
+// console.log(str.includes("learn"))
+
+// console.log(str.slice(6, 16))
+// console.log(str.slice(-5))
+// console.log(str.slice(-13,-3))
+
+// const str = `Hello Javascript, I am learning Javascript strings on 14`
+
+// console.log(str.substring(2,5)) /* cannot mark negative inex in substring */
+
+// const a = "Ketan";
+// const b = "Shetge";
+// const name = a + " " + b;
+// console.log(name);
+
+
+// console.log(24 + "Ketan")
+// console.log(25 + "Ketan" + 30)
+// console.log(25+30+"Ketan")
+
+
+
+// const str = `Hello Javascript, I am learning Javascript strings on 14`
+
+// console.log(str.replace("Javascript", "Python"))
+// console.log(str.replaceAll("Javascript", "Python"))
+
+
+
+// const user = " Ketan Shetge   "
+// console.log(user)
+// console.log(user.trim())
+
+
+const names = "Ketan, Rahul, Yash, Swapnil, Shubham, Nikhil";
+
+console.log(names.split(","))
