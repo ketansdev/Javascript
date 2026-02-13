890691ef42548f27a958753c2e6addc8a92f4ed8
Ketan Shetge <ketanshetgedev@gmail.com>
2026-02-13 08:14:04 +0530

    add Revision JS Operators

---
+++ Sheryians/test/test01.js
@@ -0,0 +1,127 @@
+// // console.log(22>22)
+
+// console.log(+ "10");
+// console.log("10");
+
+// let x = 10;
+// let y = 20;
+
+// if (x > 5 && y < 25) {
+//   console.log("A");
+// } else {
+//   console.log("B");
+// }
+
+
+
+// let isAdmin = true;
+// let isLoggedIn = false;
+
+// if(isAdmin || isLoggedIn){
+//     console.log("Access Granted");
+// }else{
+//     console.log("Access Denied");
+    
+// }
+
+
+// let temp = 35;
+
+// if(!(temp < 30)){
+//     console.log("Hot");
+// }else{
+//     console.log("Pleasant");
+// }
+
+
+// let a = 0;
+
+// if(a){
+//     console.log("Truthy");
+// }else{
+//     console.log("Falsy");
+// }
+
+
+
+
+
+// let score = 78;
+// let score = 25;
+
+// let grade = score >= 90 ? "A" : score >= 75 ? "B" : score >= 60 ? "C" : "Fail";
+
+// console.log(grade);
+
+
+
+
+// let points = 10;
+
+// let status = points > 100 ? "Gold" : points > 50 ? "Silver" : "Bronze";
+
+// console.log(status);
+
+
+// let loggedIn = true;
+// let hasToken = false;
+
+// let access = loggedIn && hasToken ? "Allow" : "Deny";
+
+// console.log(access);
+
+
+
+// let a = 5;
+// a++
+// console.log(a);
+
+
+// let b = 7;
+// ++b;
+// console.log(b);
+
+
+// let x = 3;
+// let y = x++;
+
+// console.log(x, y);
+
+
+
+// let p = 4;
+// let q = ++p;
+
+// console.log(p, q);
+
+
+
+
+// let m = 10;
+// console.log(m--);
+// console.log(m)
+
+
+
+
+// let n = 5;
+// let result = n++ + ++n;
+// console.log(result);
+
+
+
+
+// let likes = 100;
+
+// function likePost(){
+//     return ++likes;
+// }
+
+// console.log(likePost());
+// console.log(likes);
+
+
+// console.log("10" + 1);
+// console.log("10" - 1);
+// console.log(true + false);
+// console.log(!!"Sheryians");
