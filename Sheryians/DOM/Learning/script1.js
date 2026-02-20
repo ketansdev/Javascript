8b3057f77db1ee2698913358250f51d018b0435e
Ketan Shetge <ketanshetgedev@gmail.com>
2026-02-20 07:00:02 +0530

    Learn DOM concepts

---
+++ Sheryians/DOM/Learning/script1.js
@@ -0,0 +1,47 @@
+// Events and event handling
+
+// const h1 = document.querySelector("h1");
+// const p = document.querySelector("p");
+
+// const doubleClick = function () {
+//   p.style.color = "pink";
+// };
+
+// h1.addEventListener("click", () => {
+//   h1.style.color = "red";
+// });
+
+// p.addEventListener("dblclick", doubleClick);
+
+// p.removeEventListener("dblclick", doubleClick);
+
+
+
+
+// const input = document.querySelector("input");
+
+// const readText = function (e) {
+// console.log(e);
+//   if (e.data !== null) {
+//     console.log(e.data);
+//   }
+// };
+
+// input.addEventListener("input", readText);
+
+
+
+
+
+
+
+// const heading = document.querySelector("#system-heading");
+// const select = document.querySelector("select");
+
+// const readOptionVal = (e) =>{
+//     console.log(e.target.value);
+//     let system = (e.target.value).toUpperCase();
+//     heading.textContent = `${system} system selected`
+// }
+
+// select.addEventListener("change", readOptionVal);
